318f8d915c87b677299ac2b0a770ff005238b5bf
Tom Jardine-McNamara <tom@itsravenous.com>
2020-01-11 21:03:56 +0000

    fetch inventory for calling user

--- functions/inventory.js
+++ functions/inventory.js
@@ -1,17 +1,21 @@
 const GoogleSpreadsheet = require("google-sheets-node-api");
 
-const getInventory = async () => {
+const getInventory = async character => {
   const spreadsheet = await new GoogleSpreadsheet(
     process.env.SHEET_ID
   ).getSpreadsheet();
-  const rows = await spreadsheet.worksheets[0].getRows();
-  return rows.map(row => row.title);
+  const inventoryWorksheet = spreadsheet.worksheets.find(
+    worksheet => worksheet.title.toLowerCase() === character.toLowerCase()
+  );
+  if (!inventoryWorksheet) return ["No items"];
+  const items = await inventoryWorksheet.getRows();
+  return items.map(item => item.title);
 };
 
 exports.getInventory = getInventory;
 
 exports.handler = async (event, context, callback) => {
-  const { text: character } = JSON.parse(
+  const { user_name } = JSON.parse(
     '{"' + event.body.replace(/&/g, '","').replace(/=/g, '":"') + '"}',
     (key, value) =>
       key === "" ? value : decodeURIComponent(value).replace(/\+/g, " ")
@@ -19,7 +23,7 @@ exports.handler = async (event, context, callback) => {
 
   callback(null, {
     statusCode: 200,
-    body: `*Inventory*\n\n${(await getInventory())
+    body: `*Inventory for ${user_name}*\n\n${(await getInventory(user_name))
       .map(row => `- ${row}`)
       .join("\n")}`
   });
