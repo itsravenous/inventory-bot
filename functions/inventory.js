4b8a3942f1461491a0835f5e55de12b32e04cda7
Tom Jardine-McNamara <tom@itsravenous.com>
2020-01-12 19:45:31 +0000

    make columns receptacles
    Closes #1

--- functions/inventory.js
+++ functions/inventory.js
@@ -8,12 +8,38 @@ const getInventory = async character => {
     worksheet => worksheet.title.toLowerCase() === character.toLowerCase()
   );
   if (!inventoryWorksheet) return ["No items"];
-  const items = await inventoryWorksheet.getRows();
-  return items.map(item => item.title);
+  const cells = await inventoryWorksheet.getCells();
+
+  const receptacles = cells
+    .filter(cell => cell.row === 1)
+    .reduce((acc, cell) => {
+      acc[cell.col] = cell.value;
+      return acc;
+    }, {});
+
+  const items = cells.reduce((acc, cell) => {
+    if (cell.row === 1) return acc;
+    acc[receptacles[cell.col]] = acc[receptacles[cell.col]] || [];
+    acc[receptacles[cell.col]].push(cell.value);
+    return acc;
+  }, {});
+  return items;
+};
+
+const inventoryToText = inventory => {
+  inventoryText = Object.entries(inventory).reduce(
+    (acc, [receptacle, items]) => {
+      acc += `*${receptacle}:*\n${items.join("\n")}\n\n`;
+      return acc;
+    },
+    ""
+  );
+
+  return inventoryText;
 };
 
 exports.getInventory = getInventory;
-
+exports.inventoryToText = inventoryToText;
 exports.handler = async (event, context, callback) => {
   const { user_name } = JSON.parse(
     '{"' + event.body.replace(/&/g, '","').replace(/=/g, '":"') + '"}',
@@ -21,10 +47,9 @@ exports.handler = async (event, context, callback) => {
       key === "" ? value : decodeURIComponent(value).replace(/\+/g, " ")
   );
 
+  const inventoryText = inventoryToText(await getInventory(user_name));
   callback(null, {
     statusCode: 200,
-    body: `*Inventory for ${user_name}*\n\n${(await getInventory(user_name))
-      .map(row => `- ${row}`)
-      .join("\n")}`
+    body: `*Inventory for ${user_name}*\n\n${inventoryText}`
   });
 };
